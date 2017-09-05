8359a1c9f95743452b7bc35f3f5765729466e35d
hui xu <HUI.XU.1@stonybrook.edu>
2017-09-05 13:31:35 -0400

    add

---
+++ main.cpp
@@ -0,0 +1,28 @@
+//
+//  main.cpp
+//  practice2
+//
+//  Created by hui xu on 04/09/2017.
+//  Copyright © 2017 hui xu. All rights reserved.
+//
+
+#include <iostream>
+#include<valarray>
+#include<cstdlib>
+const int SIZE=12;
+
+typedef std::valarray<int> vint;
+void sshow(const int & v, int cols);
+
+
+int main()
+{
+    using std::slice;
+    using std::cout;
+    vint valint(SIZE);
+    int i;
+    for(i=0;i<SIZE;++i)
+        valint[i]=std::rand()%10;
+    how(valint,3);
+    
+    
